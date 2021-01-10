da34cd5779db0a9c8d92154f6f08a62c56fbf3f7
IslamAsim <islam.m.asim@gmail.com>
2021-01-10 16:11:17 +0200

    a5er 7aga footer

--- cars/src/app/app.module.ts
+++ cars/src/app/app.module.ts
@@ -5,16 +5,19 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import {HttpClientModule} from '@angular/common/http';
 import { LayoutComponent } from './components/layout/layout.component'
+import {NgSelectModule} from '@ng-select/ng-select';
+import {FormsModule} from '@angular/forms';
 @NgModule({
   declarations: [
     AppComponent,
     LayoutComponent
-
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     HttpClientModule,
+    NgSelectModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- cars/src/app/components/layout/layout.component.ts
+++ cars/src/app/components/layout/layout.component.ts
@@ -7,13 +7,18 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LayoutComponent implements OnInit {
 
-  constructor() { }
+  constructor() {
+  }
   IsMenuOpen = true;
+  CarBrand = ['BMW', 'Fiat', 'Honda', 'Mercedes', 'Mazda', 'KIA', 'Alfa', 'Audi', 'BYD', 'Chevrolet', 'Citroen', 'Ford', 'Hyundai', 'Jeep', 'Land-Rover', 'Mitsubishi', 'Opel', 'Peugeot', 'Porche', 'Renault', 'Skoda', 'Subaru', 'Suzuki', 'Toyota', 'Volvo'];
+  selectedCarBrand: any;
+  CarModel: [];
+  selectedCarModel: any;
+  CarPartName: [];
   ngOnInit(): void {
   }
   ToggleMenu(): boolean {
     this.IsMenuOpen = !this.IsMenuOpen;
     return this.IsMenuOpen;
   }
-
 }
