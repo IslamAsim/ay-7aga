f0d4c5f4ee2d40f94a5bd062525d2b8202a618c5
IslamAsim <islam.m.asim@gmail.com>
2021-01-11 22:48:59 +0200

    assdjsdofjdsf

--- cars/src/app/components/user/user.module.ts
+++ cars/src/app/components/user/user.module.ts
@@ -1,19 +1,22 @@
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { LoginComponent } from './login/login.component';
-import { Routes, RouterModule } from '@angular/router';
+import {NgModule} from '@angular/core';
+import {CommonModule} from '@angular/common';
+import {LoginComponent} from './login/login.component';
+import {Routes, RouterModule} from '@angular/router';
 import {ReactiveFormsModule} from '@angular/forms';
+import {ProfileComponent} from './profile/profile.component';
 
-const routes:Routes=[
-{path:'login',component:LoginComponent}
+const routes: Routes = [
+  {path: 'login', component: LoginComponent},
+  {path: 'profile', component: ProfileComponent}
 ];
 
 @NgModule({
   declarations: [LoginComponent],
   imports: [
-  CommonModule,
+    CommonModule,
     RouterModule.forChild(routes),
     ReactiveFormsModule,
   ]
 })
-export class UserModule { }
+export class UserModule {
+}
