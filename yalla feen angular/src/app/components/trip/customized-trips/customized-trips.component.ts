b747ad46fd2413d6df1218c38458f6fd63873567
IslamAsim <islam.m.asim@gmail.com>
2021-01-31 21:20:58 +0200

    get by id

---
+++ yalla feen angular/src/app/components/trip/customized-trips/customized-trips.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-customized-trips',
+  templateUrl: './customized-trips.component.html',
+  styleUrls: ['./customized-trips.component.scss']
+})
+export class CustomizedTripsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
